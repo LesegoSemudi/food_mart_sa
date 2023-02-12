ae890a05102d4a3eebf20c25bd86f0222aaa263e
Lejay <lesego777@gmail.com>
2023-02-12 03:40:22 +0200

    update

---
+++ lib/data/product_json.dart
@@ -0,0 +1,89 @@
+const List productItems = [
+  {
+    "name": "Bacon Egg Cheese Biscuit Meal",
+    "image":
+        "https://d1ralsognjng37.cloudfront.net/3915dacd-505e-48ed-81d6-a0eea9c067cf.jpeg",
+    "price": "\R 50.45",
+    "discount": "\R 60.75"
+  },
+  {
+    "name": "Big Breakfast with Hotcakes",
+    "image":
+        "https://d1ralsognjng37.cloudfront.net/a456fb02-790a-465c-8a8d-814d2904eb7a.jpeg",
+    "price": "\R 50.99",
+    "discount": "\R 70.21"
+  },
+  {
+    "name": "Hash Browns",
+    "image":
+        "https://d1ralsognjng37.cloudfront.net/41b09216-332b-4d65-a705-ccb3ad0f3964.jpeg",
+    "price": "\R 10.46",
+    "discount": "\R 30.65"
+  },
+  {
+    "name": "Big Breakfast",
+    "image":
+        "https://d1ralsognjng37.cloudfront.net/6d9d7a46-6283-4627-ae51-436a96c03e76.jpeg",
+    "price": "\R 50.69",
+    "discount": "\R 70.00"
+  },
+  {
+    "name": "Apple Pie",
+    "image":
+        "https://d1ralsognjng37.cloudfront.net/a5af8311-9b41-403a-94fe-30c2b166b39b.jpeg",
+    "price": "\R 10.49",
+    "discount": "\R 30.00"
+  },
+  {
+    "name": "2 Cookies",
+    "image":
+        "https://d1ralsognjng37.cloudfront.net/c4273c09-614c-4624-9885-e438d126ce9f.jpeg",
+    "price": "\R 10.29",
+    "discount": "\R 20.50"
+  },
+];
+
+const List drinkItems = [
+  {
+    "name": "Blue Curacao Soda",
+    "image":
+        "https://images.deliveryhero.io/image/fd-kh/Products/37749.jpg?width=2000",
+    "price": "\R 20.21",
+    "discount": "\R 20.60"
+  },
+  {
+    "name": "Cappuccino",
+    "image":
+        "https://images.deliveryhero.io/image/fd-kh/Products/37701.jpg?width=2000",
+    "price": "\R 10.91",
+    "discount": "\R 20.25"
+  },
+  {
+    "name": "Green Tea Latte",
+    "image":
+        "https://images.deliveryhero.io/image/fd-kh/Products/37707.jpg?width=2000",
+    "price": "\R 20.13",
+    "discount": "\R 20.50"
+  },
+  {
+    "name": "Americano",
+    "image":
+        "https://images.deliveryhero.io/image/fd-kh/Products/37697.jpg?width=2000",
+    "price": "\R 10.60",
+    "discount": "\R 10.90"
+  },
+  {
+    "name": "Mocha",
+    "image":
+        "https://images.deliveryhero.io/image/fd-kh/Products/37702.jpg?width=2000",
+    "price": "\R 20.13",
+    "discount": "\R 20.60"
+  },
+  {
+    "name": "Strawberry Smoothie",
+    "image":
+        "https://images.deliveryhero.io/image/fd-kh/Products/37685.jpg?width=2000",
+    "price": "\R 20.29",
+    "discount": "\R 20.70"
+  },
+];
---
+++ lib/theme/colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+const Color primary = Color.fromARGB(255, 0, 0, 0);
+const Color secondary = Color(0xFF39B54A);
+const Color success = Color(0xFF2ecc71);
+const Color danger = Color(0xFFe74c3c);
+const Color black = Color.fromARGB(255, 0, 0, 0);
+const Color light = Color(0xFFE6E9ED);
+const Color textWhite = Color(0xFFFFFFFF);
+const Color textBlack = Color(0xFF2B3849);
+const Color textFieldBg = Color(0xFFE6E9ED);
